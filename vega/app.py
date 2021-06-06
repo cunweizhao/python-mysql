062ce5104952be024dcf35a0f74cf3076721d551
cunweizhao <57098973+cunweizhao@users.noreply.github.com>
2021-06-06 16:09:59 +0800

    1.0
    Test

--- vega/app.py
+++ vega/app.py
@@ -10,6 +10,7 @@ from colorama import Fore, Style
 from getpass import getpass
 from service.user_service import UserService
 import os
+import sys
 
 __user_service = UserService()
 
@@ -22,3 +23,26 @@ while True:
     print(Fore.LIGHTGREEN_EX, "\n\t2.退出系统")
     print(Style.RESET_ALL)
     opt = input("\n\t输入操作编号：")
+    if opt == "1":
+        username = input("\n\t用户名:")
+        password = getpass("\n\t密码：")
+        result = __user_service.login(username, password)
+        # 登录成功
+        if result == True:
+            # 查询角色
+            role = __user_service.search_user_role(username)
+            os.system("cls")
+            while True:
+                if role == "新闻编辑":
+                    print("test")
+                elif role == "管理员":
+                    print(Fore.LIGHTGREEN_EX, "\n\t1.新闻管理")
+                    print(Fore.LIGHTGREEN_EX, "\n\t2.用户管理")
+                    print(Fore.LIGHTRED_EX, "\n\tabck.退出登录")
+                    print(Fore.LIGHTRED_Ex, "\n\texit.退出系统")
+                    print(Style.RESET_ALL)
+                    opt = input("\n\t输入操作编号:")
+        else:
+            print("\n\t登录失败")
+    elif opt == "2":
+        sys.exit(0)
